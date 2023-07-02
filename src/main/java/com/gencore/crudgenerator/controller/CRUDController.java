196e9161495df583513688df6fe1fc50a0f3c1c3
victorEsantos <victor.eduardo.sntos@gmail.com>
2023-07-02 19:51:20 -0300

    health check

--- src/main/java/com/gencore/crudgenerator/controller/CRUDController.java
+++ src/main/java/com/gencore/crudgenerator/controller/CRUDController.java
@@ -6,16 +6,21 @@ import com.gencore.crudgenerator.service.ControllerGenService;
 import com.gencore.crudgenerator.service.EntityGenService;
 import com.gencore.crudgenerator.service.RepositoryGenService;
 import com.gencore.crudgenerator.service.ServiceGenService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import static com.gencore.crudgenerator.service.ControllerGenService.generateControllerCode;
-
 @Controller
 public class CRUDController {
+
+    //health check
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok().build();
+    }
 
     @GetMapping("/crud")
     public String crudForm(Model model) {
