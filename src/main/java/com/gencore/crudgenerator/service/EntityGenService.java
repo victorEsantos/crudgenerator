c0f2e9e48a6f598d5e56119101399306369df5a5
victorEsantos <victor.eduardo.sntos@gmail.com>
2023-07-13 20:56:26 -0300

    ajustes styles css

--- src/main/java/com/gencore/crudgenerator/service/EntityGenService.java
+++ src/main/java/com/gencore/crudgenerator/service/EntityGenService.java
@@ -1,42 +1,69 @@
 package com.gencore.crudgenerator.service;
 
-
 import com.gencore.crudgenerator.controller.dto.EntityModel;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 public class EntityGenService {
     public static String generate(EntityModel entityModel) {
-        StringBuilder codeBuilder = new StringBuilder();
-        codeBuilder.append("import javax.persistence.Entity;\n");
-        codeBuilder.append("import javax.persistence.GeneratedValue;\n");
-        codeBuilder.append("import javax.persistence.GenerationType;\n");
-        codeBuilder.append("import javax.persistence.Id;\n\n");
-        codeBuilder.append("@Entity\n");
-        codeBuilder.append("public class ").append(entityModel.getEntityName()).append(" {\n\n");
-        codeBuilder.append("\t@Id\n");
-        codeBuilder.append("\t@GeneratedValue(strategy = GenerationType.IDENTITY)\n");
-        codeBuilder.append("\tprivate Long id;\n");
+
+        String entityBase = """
+                import javax.persistence.Entity;
+                import javax.persistence.GeneratedValue;
+                import javax.persistence.GenerationType;
+                import javax.persistence.Id;
+                                    
+                @Entity
+                public class ${entidade} {
+                                    
+                \t@Id
+                \t@GeneratedValue(strategy = GenerationType.IDENTITY)
+                \tprivate Long id;
+                                    
+                    ${atributos}
+                                    
+                    ${gettersSetters}
+                                    
+                }""";
+
+        entityBase = entityBase.replace("${entidade}", entityModel.getEntityName());
+        entityBase = entityBase.replace("${atributos}", buildAtributos(entityModel.getFields()));
+        entityBase = entityBase.replace("${gettersSetters}", buildGettersAndSetters(entityModel));
+
+        return entityBase;
+    }
+
+    private static String buildGettersAndSetters(EntityModel entityModel) {
+
+        StringBuilder gettersSetters = new StringBuilder();
 
         for (EntityModel.FieldModel field : entityModel.getFields()) {
-            if(field.getFieldType() == null) continue;
-            codeBuilder.append("\tprivate ").append(field.getFieldType().getDescricao()).append(" ").append(field.getFieldName()).append(";\n");
+            if (isNull(field.getFieldType()) || isNull(field.getFieldName())) continue;
+
+            String fieldName = field.getFieldName();
+
+            gettersSetters.append("\tpublic ").append(field.getFieldType().getDescricao()).append(" get").append(fieldName.substring(0, 1).toUpperCase()).append(fieldName.substring(1)).append("() {\n");
+            gettersSetters.append("\t\treturn ").append(fieldName).append(";\n");
+            gettersSetters.append("\t}\n\n");
+            gettersSetters.append("\tpublic void set").append(fieldName.substring(0, 1).toUpperCase()).append(fieldName.substring(1)).append("(").append(field.getFieldType().getDescricao()).append(" ").append(fieldName).append(") {\n");
+            gettersSetters.append("\t\tthis.").append(fieldName).append(" = ").append(fieldName).append(";\n");
+            gettersSetters.append("\t}\n");
         }
 
-        codeBuilder.append("\n");
-        codeBuilder.append("\t// Getters and Setters\n");
+        return gettersSetters.toString();
+    }
 
-        for (EntityModel.FieldModel field : entityModel.getFields()) {
-            if(field.getFieldType() == null) continue;
-            codeBuilder.append("\tpublic ").append(field.getFieldType().getDescricao()).append(" get").append(field.getFieldName().substring(0, 1).toUpperCase()).append(field.getFieldName().substring(1)).append("() {\n");
-            codeBuilder.append("\t\treturn ").append(field.getFieldName()).append(";\n");
-            codeBuilder.append("\t}\n\n");
-            codeBuilder.append("\tpublic void set").append(field.getFieldName().substring(0, 1).toUpperCase()).append(field.getFieldName().substring(1)).append("(").append(field.getFieldType().getDescricao()).append(" ").append(field.getFieldName()).append(") {\n");
-            codeBuilder.append("\t\tthis.").append(field.getFieldName()).append(" = ").append(field.getFieldName()).append(";\n");
-            codeBuilder.append("\t}\n\n");
+    private static String buildAtributos(List<EntityModel.FieldModel> fields) {
+        StringBuilder atributos = new StringBuilder();
+
+        for (EntityModel.FieldModel field : fields) {
+            if (isNull(field.getFieldType()) || isNull(field.getFieldName())) continue;
+
+            atributos.append("\tprivate ").append(field.getFieldType().getDescricao()).append(" ").append(field.getFieldName()).append(";\n\n");
         }
 
-
-        codeBuilder.append("\t// ...\n\n");
-        codeBuilder.append("}");
-        return codeBuilder.toString();
+        return atributos.toString();
     }
 }
